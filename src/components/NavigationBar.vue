853db6d7d8a185de98092b283841fc44f468ca41
alrwest <105620482+alrwest@users.noreply.github.com>
2023-12-08 23:21:25 -0500

    uploading src folder

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,16 @@
+
+import { RouterLink } from 'vue-router';
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to ="/"> Home </RouterLink>
+            <RouterLink to = "/UserSessions"> User Sessions </RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style>
+  
+
+</style>
